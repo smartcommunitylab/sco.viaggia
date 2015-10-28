118d3883c0ebb2f22aa6e1b4a0983b759887b942
Роман <kazhamiakin@gmail.com>
2015-10-28 20:48:52 +0100

    Timetable from real data (w/o db)

--- viaggia-mobile/www/js/controllers/Table1Ctrl.js
+++ viaggia-mobile/www/js/controllers/Table1Ctrl.js
@@ -1,6 +1,6 @@
 angular.module('viaggia.controllers.table1', ['ionic', 'ngcTableDirective'])
 
-.controller('Table1Ctrl', function ($scope, $ionicPosition) {
+.controller('Table1Ctrl', function ($scope, $ionicPosition, ttService, Config) {
     $scope.data = [];
     $scope.row_number = Math.floor((window.innerHeight - 40 - 44) / 30);
     console.log("window height" + window.innerHeight);
@@ -11,28 +11,45 @@ angular.module('viaggia.controllers.table1', ['ionic', 'ngcTableDirective'])
     $scope.column_number = window.innerWidth / 50;
     var n = 50;
 
-    for (var row = 0; row < n; row++) {
-        var rowContent = [];
-        for (var col = 0; col < n; col++) {
-            if (col == 0 && row == 0) {
-                rowContent.push("stops");
-            } else if (col == 0) {
-                rowContent.push("stop" + row);
-            } else if (row == 0) {
-                rowContent.push("course" + col);
-            } else {
-                rowContent.push(col + ":" + row);
-            }
-        }
-        $scope.data.push(rowContent);
-    }
+    $scope.tt = null;
+    $scope.getTT = function(date) {
+      Config.loading();
+      ttService.getTT('12','05A',date).then(function(data) {
+        constructTable(data);
+        Config.loaded();
+      }, function(err) {
+        $scope.tt = null;
+        Config.loaded();
+      });
+    };
+    var constructTable = function(data) {
+      var rows = [];
+      for (var row = 0; row < data.stops.length; row++) {
+          var rowContent = [];
+          for (var col = 0; col < data.tripIds.length; col++) {
+              if (col == 0 && row == 0) {
+                  rowContent.push("stops");
+              } else if (col == 0) {
+                  rowContent.push(data.stops[row]);
+              } else if (row == 0) {
+                  rowContent.push('');
+              } else {
+                  rowContent.push(data.times[col-1][row-1]);
+              }
+          }
+          rows.push(rowContent);
+      }
+//      $scope.column_number = data.tripIds.length;
+//      $scope.row_number = data.stops.length;
+      $scope.data = rows;
+      $scope.tt = data;
+    };
 
-    //    $scope.customDataFn = function (data, row, col) {
-    //        return data[n - row - 1][n - col - 1];
-    //    }
+    $scope.getTT(new Date().getTime());
+
     $scope.customDataFn = function (data, row, col) {
         if (data[row] && data[row][col])
             return data[row][col];
-        else return "yabba"
+        else return ""
     }
 });
