a0407cccd7d803ed32304e18cf73f6d609b2b519
Matteo Chini <matteo.chini@gmail.com>
2015-11-12 12:02:57 +0100

    fix issue with map related to id

--- viaggia-mobile/www/js/controllers/InfoCtrl.js
+++ viaggia-mobile/www/js/controllers/InfoCtrl.js
@@ -1,230 +1,229 @@
 angular.module('viaggia.controllers.info', [])
 
-.controller('InfoCtrl', function ($scope) {
-})
+.controller('InfoCtrl', function ($scope) {})
 
 .controller('ParkingCtrl', function ($scope, $stateParams, $timeout, $filter, $ionicModal, $ionicPopup, leafletData, mapService, parkingService, Config) {
-  $scope.agencyId = $stateParams.agencyId;
-  $scope.parkings = null;
+    $scope.agencyId = $stateParams.agencyId;
+    $scope.parkings = null;
 
-  $scope.markers = [];
+    $scope.markers = [];
 
-  $scope.title = $filter('translate')('menu_real_time_park');
+    $scope.title = $filter('translate')('menu_real_time_park');
 
-  $scope.load = function() {
-    parkingService.getParkings($scope.agencyId).then(function(data){
-      $scope.parkings = data;
-      $scope.parkings.forEach(function(e) {
-        if (e.monitored && e.slotsAvailable > -2) {
-          e.availLevel = e.slotsAvailable <= 5 ? 'avail-red' : e.slotsAvailable > 20 ? 'avail-green' : 'avail-yellow';
-        }
-      });
-      Config.loaded();
-      $scope.$broadcast('scroll.refreshComplete');
-    }, function (err) {
-      $scope.parkings = null;
-      $scope.$broadcast('scroll.refreshComplete');
-      Config.loaded();
-    });
-  }
-
-  var init = function() {
-    Config.loading();
-    $scope.load();
-  };
-
-  $scope.selected = null;
-  $scope.select = function(p) {
-    if ($scope.selected == p) $scope.selected = null;
-    else $scope.selected = p;
-  };
-
-  $scope.showMap = function() {
-    $scope.modalMap.show().then(function() {
-      var markers = [];
-
-      var list = $scope.selected != null ? [$scope.selected] : $scope.parkings;
-      if (list == null) list = [];
-      var boundsArray = [];
-      for (var i = 0; i < list.length; i++) {
-          markers.push({
-              parking: list[i],
-              lat: parseFloat(list[i].position[0]),
-              lng: parseFloat(list[i].position[1]),
-              icon: {
-                  iconUrl: 'img/ic_parkingLot.png',
-                  iconSize: [36, 50],
-                  iconAnchor: [18, 50],
-                  popupAnchor: [-0, -50]
-              },
-              //                        focus: true
-          });
-          boundsArray.push(list[i].position);
-      }
-      if (boundsArray.length > 0) {
-        var bounds = L.latLngBounds(boundsArray);
-        leafletData.getMap('mapModal').then(function(map) {
-          map.fitBounds(bounds);
+    $scope.load = function () {
+        parkingService.getParkings($scope.agencyId).then(function (data) {
+            $scope.parkings = data;
+            $scope.parkings.forEach(function (e) {
+                if (e.monitored && e.slotsAvailable > -2) {
+                    e.availLevel = e.slotsAvailable <= 5 ? 'avail-red' : e.slotsAvailable > 20 ? 'avail-green' : 'avail-yellow';
+                }
+            });
+            Config.loaded();
+            $scope.$broadcast('scroll.refreshComplete');
+        }, function (err) {
+            $scope.parkings = null;
+            $scope.$broadcast('scroll.refreshComplete');
+            Config.loaded();
         });
-      }
-      $scope.markers = markers;
+    }
+
+    var init = function () {
+        Config.loading();
+        $scope.load();
+    };
+
+    $scope.selected = null;
+    $scope.select = function (p) {
+        if ($scope.selected == p) $scope.selected = null;
+        else $scope.selected = p;
+    };
+
+    $scope.showMap = function () {
+        $scope.modalMap.show().then(function () {
+            var markers = [];
+
+            var list = $scope.selected != null ? [$scope.selected] : $scope.parkings;
+            if (list == null) list = [];
+            var boundsArray = [];
+            for (var i = 0; i < list.length; i++) {
+                markers.push({
+                    parking: list[i],
+                    lat: parseFloat(list[i].position[0]),
+                    lng: parseFloat(list[i].position[1]),
+                    icon: {
+                        iconUrl: 'img/ic_parkingLot.png',
+                        iconSize: [36, 50],
+                        iconAnchor: [18, 50],
+                        popupAnchor: [-0, -50]
+                    },
+                    //                        focus: true
+                });
+                boundsArray.push(list[i].position);
+            }
+            if (boundsArray.length > 0) {
+                var bounds = L.latLngBounds(boundsArray);
+                leafletData.getMap().then(function (map) {
+                    map.fitBounds(bounds);
+                });
+            }
+            $scope.markers = markers;
+        });
+    };
+
+    $ionicModal.fromTemplateUrl('templates/mapModal.html', {
+        id: '1',
+        scope: $scope,
+        backdropClickToClose: false,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modalMap = modal;
     });
-  };
 
-  $ionicModal.fromTemplateUrl('templates/mapModal.html', {
-      id: '1',
-      scope: $scope,
-      backdropClickToClose: false,
-      animation: 'slide-in-up'
-  }).then(function (modal) {
-      $scope.modalMap = modal;
-  });
-
-  angular.extend($scope, {
-    center: {},
-    markers: [],
-    events: {}
-  });
-
-  $scope.closeMap = function() {
-    $scope.modalMap.hide();
-  };
-  $scope.initMap = function() {
-    mapService.initMap().then(function () {
+    angular.extend($scope, {
+        center: {},
+        markers: [],
+        events: {}
     });
-  };
 
-  $scope.$on('leafletDirectiveMarker.mapModal.click',function(e,args){
-    var p = $scope.markers[args.modelName].parking;
-    $scope.popupParking = p;
-    $ionicPopup.show({
-      templateUrl: 'templates/parkingPopup.html',
-      title: $filter('translate')('lbl_parking'),
-      cssClass: 'parking-popup',
-      scope: $scope,
-      buttons: [
-        { text: $filter('translate')('btn_close')},
-        {
-          text: $filter('translate')('btn_nav_to'),
-          onTap: function(e) {
-            // TODO
-          }
+    $scope.closeMap = function () {
+        $scope.modalMap.hide();
+    };
+    $scope.initMap = function () {
+        mapService.initMap().then(function () {});
+    };
+
+    $scope.$on('leafletDirectiveMarker.mapModal.click', function (e, args) {
+        var p = $scope.markers[args.modelName].parking;
+        $scope.popupParking = p;
+        $ionicPopup.show({
+            templateUrl: 'templates/parkingPopup.html',
+            title: $filter('translate')('lbl_parking'),
+            cssClass: 'parking-popup',
+            scope: $scope,
+            buttons: [
+                {
+                    text: $filter('translate')('btn_close')
+                },
+                {
+                    text: $filter('translate')('btn_nav_to'),
+                    onTap: function (e) {
+                        // TODO
+                    }
         }
       ]
-    }
-  );
-  });
+        });
+    });
 
-  $timeout(init,200);
+    $timeout(init, 200);
 })
 
 
 .controller('BikeSharingCtrl', function ($scope, $stateParams, $timeout, $filter, $ionicModal, $ionicPopup, leafletData, mapService, bikeSharingService, Config) {
-  $scope.agencyId = $stateParams.agencyId;
-  $scope.parkings = null;
+    $scope.agencyId = $stateParams.agencyId;
+    $scope.parkings = null;
 
-  $scope.markers = [];
+    $scope.markers = [];
 
-  $scope.title = $filter('translate')('menu_real_time_bike');
+    $scope.title = $filter('translate')('menu_real_time_bike');
 
-  $scope.load = function() {
-    bikeSharingService.getStations($scope.agencyId).then(function(data){
-      $scope.parkings = data;
-      Config.loaded();
-      $scope.$broadcast('scroll.refreshComplete');
-    }, function (err) {
-      $scope.parkings = null;
-      $scope.$broadcast('scroll.refreshComplete');
-      Config.loaded();
-    });
-  }
-
-  var init = function() {
-    Config.loading();
-    $scope.load();
-  };
-
-  $scope.selected = null;
-  $scope.select = function(p) {
-    if ($scope.selected == p) $scope.selected = null;
-    else $scope.selected = p;
-  };
-
-  $scope.showMap = function() {
-    $scope.modalMap.show().then(function() {
-      var markers = [];
-
-      var list = $scope.selected != null ? [$scope.selected] : $scope.parkings;
-      if (list == null) list = [];
-      var boundsArray = [];
-      for (var i = 0; i < list.length; i++) {
-          markers.push({
-              parking: list[i],
-              lat: parseFloat(list[i].position[0]),
-              lng: parseFloat(list[i].position[1]),
-              icon: {
-                  iconUrl: 'img/ic_bike.png',
-                  iconSize: [36, 50],
-                  iconAnchor: [18, 50],
-                  popupAnchor: [-0, -50]
-              },
-              //                        focus: true
-          });
-          boundsArray.push(list[i].position);
-      }
-      if (boundsArray.length > 0) {
-        var bounds = L.latLngBounds(boundsArray);
-        leafletData.getMap('mapModal').then(function(map) {
-          map.fitBounds(bounds);
+    $scope.load = function () {
+        bikeSharingService.getStations($scope.agencyId).then(function (data) {
+            $scope.parkings = data;
+            Config.loaded();
+            $scope.$broadcast('scroll.refreshComplete');
+        }, function (err) {
+            $scope.parkings = null;
+            $scope.$broadcast('scroll.refreshComplete');
+            Config.loaded();
         });
-      }
-      $scope.markers = markers;
+    }
+
+    var init = function () {
+        Config.loading();
+        $scope.load();
+    };
+
+    $scope.selected = null;
+    $scope.select = function (p) {
+        if ($scope.selected == p) $scope.selected = null;
+        else $scope.selected = p;
+    };
+
+    $scope.showMap = function () {
+        $scope.modalMap.show().then(function () {
+            var markers = [];
+
+            var list = $scope.selected != null ? [$scope.selected] : $scope.parkings;
+            if (list == null) list = [];
+            var boundsArray = [];
+            for (var i = 0; i < list.length; i++) {
+                markers.push({
+                    parking: list[i],
+                    lat: parseFloat(list[i].position[0]),
+                    lng: parseFloat(list[i].position[1]),
+                    icon: {
+                        iconUrl: 'img/ic_bike.png',
+                        iconSize: [36, 50],
+                        iconAnchor: [18, 50],
+                        popupAnchor: [-0, -50]
+                    },
+                    //                        focus: true
+                });
+                boundsArray.push(list[i].position);
+            }
+            if (boundsArray.length > 0) {
+                var bounds = L.latLngBounds(boundsArray);
+                leafletData.getMap().then(function (map) {
+                    map.fitBounds(bounds);
+                });
+            }
+            $scope.markers = markers;
+        });
+    };
+
+    $ionicModal.fromTemplateUrl('templates/mapModal.html', {
+        id: '1',
+        scope: $scope,
+        backdropClickToClose: false,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modalMap = modal;
     });
-  };
 
-  $ionicModal.fromTemplateUrl('templates/mapModal.html', {
-      id: '1',
-      scope: $scope,
-      backdropClickToClose: false,
-      animation: 'slide-in-up'
-  }).then(function (modal) {
-      $scope.modalMap = modal;
-  });
-
-  angular.extend($scope, {
-    center: {},
-    markers: [],
-    events: {}
-  });
-
-  $scope.closeMap = function() {
-    $scope.modalMap.hide();
-  };
-  $scope.initMap = function() {
-    mapService.initMap().then(function () {
+    angular.extend($scope, {
+        center: {},
+        markers: [],
+        events: {}
     });
-  };
 
-  $scope.$on('leafletDirectiveMarker.mapModal.click',function(e,args){
-    var p = $scope.markers[args.modelName].parking;
-    $scope.popupParking = p;
-    $ionicPopup.show({
-      templateUrl: 'templates/bikesharingPopup.html',
-      title: $filter('translate')('lbl_bike_station'),
-      cssClass: 'parking-popup',
-      scope: $scope,
-      buttons: [
-        { text: $filter('translate')('btn_close')},
-        {
-          text: $filter('translate')('btn_nav_to'),
-          onTap: function(e) {
-            // TODO
-          }
+    $scope.closeMap = function () {
+        $scope.modalMap.hide();
+    };
+    $scope.initMap = function () {
+        mapService.initMap().then(function () {});
+    };
+
+    $scope.$on('leafletDirectiveMarker.mapModal.click', function (e, args) {
+        var p = $scope.markers[args.modelName].parking;
+        $scope.popupParking = p;
+        $ionicPopup.show({
+            templateUrl: 'templates/bikesharingPopup.html',
+            title: $filter('translate')('lbl_bike_station'),
+            cssClass: 'parking-popup',
+            scope: $scope,
+            buttons: [
+                {
+                    text: $filter('translate')('btn_close')
+                },
+                {
+                    text: $filter('translate')('btn_nav_to'),
+                    onTap: function (e) {
+                        // TODO
+                    }
         }
       ]
-    }
-  );
-  });
+        });
+    });
 
-  $timeout(init,200);
+    $timeout(init, 200);
 })
