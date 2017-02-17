2a81010be3696c5a5e5af4071cc63a272e426394
chin8 <matteo.chini@fbk.eu>
2017-02-17 11:22:40 +0100

    fixed news problem with local library

--- viaggia-mobile/www/js/controllers/NewsCtrl.js
+++ viaggia-mobile/www/js/controllers/NewsCtrl.js
@@ -1,48 +1,56 @@
 angular.module('viaggia.controllers.news', [])
 
-.controller('NewsCtrl', function ($scope, $state, $ionicLoading, feedService, Config) {
+.controller('NewsCtrl', function ($scope, $state, $ionicLoading, $filter, Toast, feedService, Config) {
   $scope.news = null;
 
   var FEED_URL = Config.getRSSUrl();
   var APP_ID = Config.getAppId();
 
   $ionicLoading.show();
-  feedService.load(FEED_URL,APP_ID).then(function(entries) {
-      entries.forEach(function(entry) {
-          entry.dateTime = new Date(entry.publishedDate);
-      });
-      $scope.news = entries;
-      $ionicLoading.hide();
+  feedService.load(FEED_URL, APP_ID).then(function (entries) {
+    entries.forEach(function (entry) {
+      entry.dateTime = new Date(entry.pubDate);
+    });
+    $scope.news = entries;
+    $ionicLoading.hide();
+  }, function (err) {
+    $ionicLoading.hide();
+    Toast.show($filter('translate')('toast_error_server_template'), "short", "bottom");
   });
 
-  $scope.doRefresh = function() {
-      feedService.load(FEED_URL,APP_ID, true).then(function(entries) {
-          entries.forEach(function(entry) {
-              entry.dateTime = new Date(entry.publishedDate);
-          });
-          $scope.news = entries;
-          $scope.$broadcast('scroll.refreshComplete');
+  $scope.doRefresh = function () {
+    feedService.load(FEED_URL, APP_ID, true).then(function (entries) {
+      entries.forEach(function (entry) {
+        entry.dateTime = new Date(entry.pubDate);
       });
+      $scope.news = entries;
+      $scope.$broadcast('scroll.refreshComplete');
+    }, function (err) {
+      $scope.$broadcast('scroll.refreshComplete');
+      Toast.show($filter('translate')('toast_error_server_template'), "short", "bottom");
+    });
   };
 
-  $scope.showNews = function(idx) {
-    $state.go("app.newsitem",{id: idx});
+  $scope.showNews = function (idx) {
+    $state.go("app.newsitem", {
+      id: idx
+    });
   }
 
 })
 
 .controller('NewsItemCtrl', function ($scope, $stateParams, $rootScope, feedService, Config) {
-    var FEED_URL = Config.getRSSUrl();
-    var APP_ID = Config.getAppId();
+  var FEED_URL = Config.getRSSUrl();
+  var APP_ID = Config.getAppId();
 
-    $scope.newsItem = null;
-    $scope.idx = $stateParams.id;
-    feedService.getByIdx($scope.idx, FEED_URL,APP_ID).then(function(data){
-        $scope.newsItem = data;
-        $scope.newsItem.dateTime = new Date($scope.newsItem.publishedDate);
-    });
+  $scope.newsItem = null;
+  $scope.idx = $stateParams.id;
+  feedService.getByIdx($scope.idx, FEED_URL, APP_ID).then(function (data) {
+    $scope.newsItem = data;
+    $scope.newsItem.dateTime = new Date($scope.newsItem.pubDate);
+  });
 
-    $scope.clickLink = function(link) {
-      window.open(link, '_system', 'location=yes')
-    }
+  $scope.clickLink = function (link) {
+    window.open(link, '_system', 'location=yes')
+  }
 })
--- viaggia-mobile/www/js/services/NotificationSrv.js
+++ viaggia-mobile/www/js/services/NotificationSrv.js
@@ -6,145 +6,144 @@ angular.module('viaggia.services.notification', [])
 //
 .factory('notificationService', function ($q, $http, $rootScope, $ionicPlatform, Config) {
 
-    var notificationService = {};
-    //var numberNotification = 10;
-    notificationService.getNotifications = function (sinceTimestamp, sincePosition, numberNotification) {
-        var deferred = $q.defer();
-        $http.get(Config.getMessagingServerURL() + '/app/public/notification/' + Config.getMessagingAppId() +
-                '/?since=' + sinceTimestamp +
-                '&position=' + sincePosition +
-                '&count=' + numberNotification, Config.getHTTPConfig())
-            .success(function (data) {
-                deferred.resolve(data.notifications);
-            })
-            .error(function (err) {
-                deferred.reject(err);
+  var notificationService = {};
+  //var numberNotification = 10;
+  notificationService.getNotifications = function (sinceTimestamp, sincePosition, numberNotification) {
+    var deferred = $q.defer();
+    $http.get(Config.getMessagingServerURL() + '/app/public/notification/' + Config.getMessagingAppId() +
+        '/?since=' + sinceTimestamp +
+        '&position=' + sincePosition +
+        '&count=' + numberNotification, Config.getHTTPConfig())
+      .success(function (data) {
+        deferred.resolve(data.notifications);
+      })
+      .error(function (err) {
+        deferred.reject(err);
 
-            });
-        return deferred.promise;
-    }
+      });
+    return deferred.promise;
+  }
 
-    // Register to GCM
-    notificationService.register = function () {
-        console.log("registration")
-        var push = PushNotification.init({
-            android: {
-                senderID: Config.getSenderID(),
-                topics: [Config.getMessagingAppId()],
-                icon: "notification",
-                iconColor: "gray"
-            },
-            ios: {
-                alert: "true",
-                badge: "true",
-                sound: "true",
-                senderID: Config.getSenderID(),
-                topics: [Config.getMessagingAppId()]
-            },
-            windows: {}
-        });
+  // Register to GCM
+  notificationService.register = function () {
+    console.log("registration")
+    var push = PushNotification.init({
+      android: {
+        senderID: Config.getSenderID(),
+        topics: [Config.getMessagingAppId()],
+        icon: "notification",
+        iconColor: "gray"
+      },
+      ios: {
+        alert: "true",
+        badge: "true",
+        sound: "true",
+        senderID: Config.getSenderID(),
+        topics: [Config.getMessagingAppId()]
+      },
+      windows: {}
+    });
 
-        push.on('registration', function (data) {
-            //alert('registration' + JSON.stringify(data));
-            console.log('registration' + JSON.stringify(data));
-
-
-        });
-        //What to do when I get a notification
-        push.on('notification', function (data) {
-            //alert("notification" + JSON.stringify(data));
-            console.log("notification" + JSON.stringify(data));
-            var lastTimeUpdate = new Date(localStorage.getItem(Config.getAppId() + '_lastUpdateTime'));
-            console.log('received');
-            $rootScope.countNotification = $rootScope.countNotification + 1;
-            //alert("number of notification" + JSON.stringify($rootScope.countNotification));
-            updateNotifications(lastTimeUpdate);
-        });
+    push.on('registration', function (data) {
+      //alert('registration' + JSON.stringify(data));
+      console.log('registration' + JSON.stringify(data));
 
 
-        //...and in case of erro
-        push.on('error', function (e) {
-            //            alert('error' + e.message);
-            console.log('error' + e.message)
-
-        });
-
-
-
-
-    }
-
-    var getNotificationDetail = function () {}
-
-    //get the last notification since last update
-    var updateNotifications = function (lastTimeUpdate) {
-        $http.get(Config.getMessagingServerURL() + '/app/public/notification/' + Config.getMessagingAppId() +
-                '/?since=' + lastTimeUpdate.getTime() +
-                '&position=' + 0 +
-                '&count=' + numberNotification, Config.getHTTPConfig())
-            .success(function (data) {
-                if (data.notifications) {
-                    var lastUpdateTime = new Date(data.notifications[0].updateTime);
-                    localStorage.setItem(Config.getAppId() + '_lastUpdateTime', lastUpdateTime);
-                    //update the local notifications and not readed index
-                    localStorage.setItem(Config.getAppId() + '_notifications', JSON.stringify(data.notifications));
-                }
-                console.log('updated');
-            })
-            .error(function (err) {
+    });
+    //What to do when I get a notification
+    push.on('notification', function (data) {
+      //alert("notification" + JSON.stringify(data));
+      console.log("notification" + JSON.stringify(data));
+      var lastTimeUpdate = new Date(localStorage.getItem(Config.getAppId() + '_lastUpdateTime'));
+      console.log('received');
+      $rootScope.countNotification = $rootScope.countNotification + 1;
+      //alert("number of notification" + JSON.stringify($rootScope.countNotification));
+      updateNotifications(lastTimeUpdate);
+    });
 
 
-            });
-    }
-    return notificationService;
+    //...and in case of erro
+    push.on('error', function (e) {
+      //            alert('error' + e.message);
+      console.log('error' + e.message)
+
+    });
+
+
+
+
+  }
+
+  var getNotificationDetail = function () {}
+
+  //get the last notification since last update
+  var updateNotifications = function (lastTimeUpdate) {
+    $http.get(Config.getMessagingServerURL() + '/app/public/notification/' + Config.getMessagingAppId() +
+        '/?since=' + lastTimeUpdate.getTime() +
+        '&position=' + 0 +
+        '&count=' + numberNotification, Config.getHTTPConfig())
+      .success(function (data) {
+        if (data.notifications) {
+          var lastUpdateTime = new Date(data.notifications[0].updateTime);
+          localStorage.setItem(Config.getAppId() + '_lastUpdateTime', lastUpdateTime);
+          //update the local notifications and not readed index
+          localStorage.setItem(Config.getAppId() + '_notifications', JSON.stringify(data.notifications));
+        }
+        console.log('updated');
+      })
+      .error(function (err) {
+
+
+      });
+  }
+  return notificationService;
 })
 
 .factory('feedService', function ($q, $rootScope, $http) {
-    var cache = [];
+  var cache = [];
 
-    var load = function (url, feedKey, forceLoad) {
-        var deferred = $q.defer();
+  var load = function (url, feedKey, forceLoad) {
+    var deferred = $q.defer();
 
-        var oldTimestamp = localStorage['timestamp_' + feedKey];
-        if (!forceLoad && oldTimestamp && new Date().getTime() - 10 * 60 * 1000 < oldTimestamp) {
-            if (cache.length > 0) deferred.resolve(cache);
-            else {
-                deferred.resolve(cache = JSON.parse(localStorage['entries_' + feedKey]));
-            }
+    var oldTimestamp = localStorage['timestamp_' + feedKey];
+    if (!forceLoad && oldTimestamp && new Date().getTime() - 10 * 60 * 1000 < oldTimestamp) {
+      if (cache.length > 0) deferred.resolve(cache);
+      else {
+        deferred.resolve(cache = JSON.parse(localStorage['entries_' + feedKey]));
+      }
+    } else {
+      $http.get(url).then(function (page) {
+        //download and parse the new feeds
+        var x2js = new X2JS();
+        var jsonObj = x2js.xml_str2json(page.data);
+        if (jsonObj && jsonObj.rss && jsonObj.rss.channel && jsonObj.rss.channel.item) {
+          var res = jsonObj.rss.channel.item;
+          localStorage['entries_' + feedKey] = JSON.stringify(res);
+          localStorage['timestamp_' + feedKey] = new Date().getTime();
+          cache = res;
+          deferred.resolve(res);
         } else {
-            $http.jsonp('https://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url))
-                .success(function (data) {
-                    var res = data.responseData.feed.entries;
-                    localStorage['entries_' + feedKey] = JSON.stringify(res);
-                    localStorage['timestamp_' + feedKey] = new Date().getTime();
-                    cache = res;
-                    deferred.resolve(res);
-                })
-                .error(function (data) {
-                    console.log("ERROR: " + data);
-                    if (localStorage['entries_' + feedKey]) {
-                        var res = JSON.parse(localStorage['entries_' + feedKey]);
-                        cache = res;
-                        deferred.resolve(res);
-                    }
-                    deferred.resolve([]);
-                });
+          deferred.reject();
         }
-
-        return deferred.promise;
-    };
-
-    var getByIdx = function (idx, url, feedKey) {
-        var deferred = $q.defer();
-        load(url, feedKey).then(function (data) {
-            if (data.length > idx) deferred.resolve(data[idx]);
-            return null;
-        });
-        return deferred.promise;
-    };
-
-    return {
-        load: load,
-        getByIdx: getByIdx
+      }, function (err) {
+        deferred.reject();
+      });
     }
+
+    return deferred.promise;
+  };
+
+  var getByIdx = function (idx, url, feedKey) {
+    var deferred = $q.defer();
+    load(url, feedKey).then(function (data) {
+      if (data.length > idx) deferred.resolve(data[idx]);
+      return null;
+    });
+    return deferred.promise;
+  };
+
+  return {
+    load: load,
+    getByIdx: getByIdx
+  }
 });
