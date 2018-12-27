36d4fdbec5e481ee1352a512d3e0cafbd0d62b94
chin8 <matteo.chini@fbk.eu>
2018-12-27 10:15:42 +0100

    fixed issue of multiple network calls

--- viaggia-mobile/www/js/services/GameSrv.js
+++ viaggia-mobile/www/js/services/GameSrv.js
@@ -742,7 +742,7 @@ angular.module('viaggia.services.game', [])
       LoginService.getValidAACtoken().then(
         function (token) {
           gameService.getChallenge(token, 'PROPOSED').then(function (challenges) {
-            updateVariables(null,challenges);
+           // updateVariables(null,challenges);
             deferred.resolve(challenges);
           }, function (err) {
             deferred.reject();
@@ -1397,7 +1397,7 @@ angular.module('viaggia.services.game', [])
     gameService.updateStatus = function () {
       gameService.getLocalStatus().then(
         function (status) {
-          updateVariables(status);
+          //updateVariables(status);
         },
         function (err) {}
       );
